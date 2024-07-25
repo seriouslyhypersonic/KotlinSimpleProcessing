84d7fe7d1819f286df9a74aded4735858091e12b
seriouslyhypersonic <nuno@alvesdesousa.pt>
2024-07-25 22:04:26 +0100

    Refactor packages

--- library/src/main/java/com/seriouslyhypersonic/library/kotlin/Alias.kt
+++ library/src/main/java/com/seriouslyhypersonic/library/kotlin/Alias.kt
@@ -1,5 +1,6 @@
 package com.seriouslyhypersonic.library.kotlin
 
+import com.seriouslyhypersonic.library.kotlin.preview.PreviewSpec
 import kotlin.reflect.KClass
 
 internal typealias AnyKClass = KClass<*>
