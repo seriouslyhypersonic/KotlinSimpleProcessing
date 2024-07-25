a728d05edb7873f1ba908754ca251983f4e5f353
seriouslyhypersonic <nuno@alvesdesousa.pt>
2024-07-25 22:04:41 +0100

    Refactor packages

--- app/src/main/java/com/seriouslyhypersonic/kspforall/core/di/AppModule.kt
+++ app/src/main/java/com/seriouslyhypersonic/kspforall/core/di/AppModule.kt
@@ -1,14 +1,8 @@
 package com.seriouslyhypersonic.kspforall.core.di
 
-import com.seriouslyhypersonic.kspforall.demo.weather.data.MockWeatherService
-import com.seriouslyhypersonic.kspforall.demo.weather.data.WeatherService
 import org.koin.core.annotation.ComponentScan
 import org.koin.core.annotation.Module
-import org.koin.core.annotation.Single
 
 @Module
 @ComponentScan("com.seriouslyhypersonic.kspforall")
-class AppModule {
-    @Single
-    fun weatherService(): WeatherService = MockWeatherService()
-}
+class AppModule
