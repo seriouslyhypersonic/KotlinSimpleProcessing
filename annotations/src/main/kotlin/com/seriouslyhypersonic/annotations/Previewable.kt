51c8e6d163081d371266578e87cfcaefb74a0289
seriouslyhypersonic <nuno@alvesdesousa.pt>
2024-08-15 14:28:19 +0100

    Add Previewable documentation

--- annotations/src/main/kotlin/com/seriouslyhypersonic/annotations/Previewable.kt
+++ annotations/src/main/kotlin/com/seriouslyhypersonic/annotations/Previewable.kt
@@ -1,3 +1,47 @@
 package com.seriouslyhypersonic.annotations
 
+
+/**
+ * The [Previewable] annotation is used to generate an interface for a declared `ViewModel` which
+ * can be used to easily preview `ViewModel`s.
+ *
+ * Example:
+ * ```
+ * @Previewable
+ * class MainScreenViewModel : SomeMainScreenViewModel, ViewModel() {
+ *     override var title by mutableStateOf("MainScreen")
+ *         private set
+ *
+ *     override var greeting by mutableStateOf("Good morning!")
+ *         private set
+ *
+ *     ...
+ *
+ *     override updateGreeting() {
+ *         // update logic
+ *     }
+ * }
+ * ```
+ * will generate a `SomeMainScreenViewModel` that exposes the properties and methods marked with
+ * `override`:
+ * ```
+ * interface SomeMainScreenViewModel {
+ *     val title: String
+ *     val greeting: String
+ *     fun updateGreeting()
+ * }
+ * ```
+ * This interface can then be used to create a preview view model as a simple data class:
+ * ```
+ * data class MainScreenPreviewViewModel(
+ *     override val title: String,
+ *     override val greeting: String,
+ * ) : SomeMainScreenViewModel {
+ *     override update() { /* no-op */ }
+ * }
+ * ```
+ * The preview data
+ */
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.SOURCE)
 public annotation class Previewable(val generateModel: Boolean = true)
